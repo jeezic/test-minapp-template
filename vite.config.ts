60c5768935c91614652b4df0ca55c664b19817db
jeezic <jeezic@yandex.ru>
2025-05-01 22:21:56 +0700

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,13 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react-swc';
 import mkcert from 'vite-plugin-mkcert';
-import {readFileSync} from "fs";
-import {resolve} from "path";
-import basicSsl from '@vitejs/plugin-basic-ssl';
+// import {readFileSync} from "fs";
+// import {resolve} from "path";
+// import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/reactjs-template/',
+  base: '/',
   css: {
     preprocessorOptions: {
       scss: {
