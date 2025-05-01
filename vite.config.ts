4268f05276fe9ebee9b961127fe3016226398e3b
jeezic <jeezic@yandex.ru>
2025-05-01 22:05:54 +0700

    init

---
+++ vite.config.ts
@@ -0,0 +1,45 @@
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import react from '@vitejs/plugin-react-swc';
+import mkcert from 'vite-plugin-mkcert';
+import {readFileSync} from "fs";
+import {resolve} from "path";
+import basicSsl from '@vitejs/plugin-basic-ssl';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/reactjs-template/',
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern',
+      },
+    },
+  },
+  plugins: [
+    // Allows using React dev server along with building a React application with Vite.
+    // https://npmjs.com/package/@vitejs/plugin-react-swc
+    react(),
+    // Allows using the compilerOptions.paths property in tsconfig.json.
+    // https://www.npmjs.com/package/vite-tsconfig-paths
+    tsconfigPaths(),
+    // Creates a custom SSL certificate valid for the local machine.
+    // Using this plugin requires admin rights on the first dev-mode launch.
+    // https://www.npmjs.com/package/vite-plugin-mkcert
+    process.env.HTTPS && mkcert(),
+   // basicSsl()
+  ],
+  build: {
+    target: 'esnext',
+  },
+  publicDir: './public',
+  server: {
+    // Exposes your dev server and makes it accessible for the devices in the same network.
+    host: '0.0.0.0',
+    https: {
+      cert: readFileSync(resolve('195.211.167.209+2.pem')),
+      key: readFileSync(resolve('195.211.167.209+2-key.pem')),
+    },
+  },
+});
+
